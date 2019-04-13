3856d93a431dd98f3ff619e52d496ea96a09aff2
marcio <margeodev@gmail.com>
2019-04-13 00:11:36 -0300

    Criação do componente Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import './styles.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Welcome React</h1>
+        <Header />
       </div>
     );
   }
