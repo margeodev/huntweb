3e71d6d36d121752cdab1911204872788bdefc98
marcio <margeodev@gmail.com>
2019-04-13 01:25:52 -0300

    Criação da listagem dos produtos via api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import Main from './pages/main';
+
 import './styles.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Header />,
+    <Main />
+  </div>
+);
 
 export default App;
